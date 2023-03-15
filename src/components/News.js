bb0ff3a925d032c065f1f1d7f83ab5114a89e664
akash kushwaha <akashkushwaha76986@gmail.com>
2023-03-15 18:16:11 +0530

    API fetched

--- src/components/News.js
+++ src/components/News.js
@@ -2,24 +2,41 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-        <div className="container my-3">
-            <h2>NewsWalla - Top Headlines</h2>
-            <div className="row"> 
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="mydesc" imageUrl="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" newsUrl="TODO"/>
+    constructor() {
+        super();
+        console.log("Hello I am Constructor from News Component")
+        this.state = {
+            articles: [],
+            loading: true
+
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=dcc8c278ea5f4e38b6e2970430fbae25"
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles })
+    }
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h1>NewsWalla - Top Headlines</h1>
+
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""}
+                                description={element.description ? element.description : ""} imageUrl={element.urlToImage}
+                                newsUrl={element.url} />
+                        </div>
+                    })}
                 </div>
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="mydesc"/>
-                </div>
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="mydesc"/>
-                </div> 
-            </div> 
-        </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let {title, description} = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
-    <div className="card" style={{width: "18rem;"}}>
-        <img src="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" alt="..."/>
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={!imageUrl? "https://img.onmanorama.com/content/dam/mm/en/kerala/top-news/images/2023/1/19/high-court-kerala.jpg":imageUrl} alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <a href={newsUrl} target= "_blank" className="btn btn-sm btn-primary">Read More</a>
           </div>
-      </div>
+        </div>
       </div>
     )
   }
