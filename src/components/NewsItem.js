3d585eed42d89c81cb6c4d3f829b11aca8fd1478
akash kushwaha <akashkushwaha76986@gmail.com>
2023-03-18 21:32:54 +0530

    newsapp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={!imageUrl? "https://img.onmanorama.com/content/dam/mm/en/kerala/top-news/images/2023/1/19/high-court-kerala.jpg":imageUrl} alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target= "_blank" className="btn btn-sm btn-primary">Read More</a>
+            <a rel="noreferrer" href={newsUrl} target= "_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
@@ -19,4 +19,4 @@ export class NewsItem extends Component {
   }
 }
 
-export default NewsItem
+export default NewsItem 
